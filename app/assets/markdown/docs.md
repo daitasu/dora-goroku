eacb5edaf1f88fe2b9b932b511638270889f640c
daitasu <ifctp.dai.monara@gmail.com>
2020-09-12 22:18:58 +0900

    Add api docs page

---
+++ app/assets/markdown/docs.md
@@ -0,0 +1,30 @@
+﻿Dora-Goroku is a public service api that serve about Doraemon quotes.
+
+## GET /api/v1/quotes/random
+### Response
+```
+{
+
+}
+```
+
+## GET /api/v1/quotes
+### Parameters
+
+| Name | Description |
+| ------ | ------ |
+| keyword(query) | Keyword value that need to be considered for filter |
+
+### Example
+
+```
+https://hogehoge.com/api/v1/quotes?keyword=人生
+```
+
+### Response
+
+```
+{
+
+}
+```
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,22 @@
+
+
 module StaticPagesHelper
+  def markdown
+
+    docs = File.read("#{Rails.root}/app/assets/markdown/docs.md")
+
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      lax_html_blocks: true,
+      strikethrough: true
+    }
+    renderer = Redcarpet::Render::HTML.new()
+    markdown = Redcarpet::Markdown.new(renderer, options)
+
+    markdown.render(docs).html_safe
+  end
 end
