0ba818acc15f2d088dc36732c64b2114221c0a50
daitasu <ifctp.dai.monara@gmail.com>
2020-09-23 23:41:18 +0900

    Add keyword params for search

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -2,14 +2,14 @@ module Api
   module V1
     class QuotesController < ApplicationController
       def index
-        quotes = Quote.order(created_at: :desc).map do |quote|
+        quotes = Quote.where("quote like ?","%#{params[:keyword]}%").order(created_at: :desc).map do |quote|
           quote.slice(:character, :quote)
         end
         render json: quotes
       end
 
       def random
-        quote = Quote.order(Arel.sql('RANDOM()')).first
+        quote = Quote.order(Arel.sql("RANDOM()")).first
         render json: quote.slice(:character, :quote)
       end
     end
